ce16398d0b6acaa3274340f749577dad0b87a5e5
Diyor <baxacap@gmail.com>
2024-11-09 14:09:14 +0500

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,62 +1,132 @@
 import React, { useState } from "react";
 import "./Navbar.css";
-import logo from './img/logo.png'
+import logo from "./img/logo.png";
 
 const Navbar = () => {
-  const [dropdown, setDropdown] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  // Toggle function for the mobile menu
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
-    <nav className="navbar">
-      <div className="navbar-logo">
-        <img src={logo} alt="logo" />
-      </div>
-      <ul className="navbar-links">
-        <li>
-          <a href="#home">About Us</a>
-        </li>
-        <li>
-          <a href="#about">Team</a>
-        </li>
-        <li>
-          <a href="#services">Services</a>
-        </li>
-        <li>
-          <a href="#projects">Projects</a>
-        </li>
-        <li>
-          <a href="#questions">Questions</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-        <li
-          className="dropdown"
-          onMouseEnter={() => setDropdown(true)}
-          onMouseLeave={() => setDropdown(false)}
-        >
-          <a href="#more">Language</a>
-          {dropdown && (
-            <ul className="dropdown-menu">
-              <li>
-                <a href="#en">EN</a>
+    <div className="container-fluid c">
+      <nav className="navbar navbar-expand-lg ">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img src={logo} alt="logo" className="logo" />
+          </a>
+          <button
+            className="navbar-toggler link"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span>☰</span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 list">
+              <li className="nav-item">
+                <a className="link" aria-current="page" href="#">
+                  About Us
+                </a>
               </li>
-              <li>
-                <a href="#ru">RU</a>
+              <li className="nav-item">
+                <a className="link" href="#">
+                  Team
+                </a>
               </li>
-              <li>
-                <a href="#uz">UZ</a>
+              <li className="nav-item">
+                <a className="link" href="#">
+                  Services
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="link" href="#">
+                  Project
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="link" href="#">
+                  Questions
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="link" href="#">
+                  Contact
+                </a>
               </li>
             </ul>
-          )}
-        </li>
-        <li>
-          <a href="#share">Share</a>
-        </li>
-      </ul>
-      <div className="navbar-toggle" onClick={() => setDropdown(!dropdown)}>
-        ☰
-      </div>
-    </nav>
+            <div className="d-flex gap-5" role="search">
+              <li className="nav-item dropdown">
+                <a
+                  className="link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Languages
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a className="dropdown-item" href="#uz">
+                      UZ
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#ru">
+                      RU
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item m-8" href="#en">
+                      EN
+                    </a>
+                  </li>
+                </ul>
+              </li>
+
+              <li className="nav-item dropdown">
+                <a
+                  className="link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu d">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 };
 
