b191214ad0c39a97768be07d173fca46d55ffc0c
Diyor <baxacap@gmail.com>
2024-11-09 13:03:53 +0500

    navbar with media screen

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,88 +1,63 @@
 import React, { useState } from "react";
 import "./Navbar.css";
-import logo from "./img/logo.png";
+import logo from './img/logo.png'
 
-function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [selected, setSelected] = useState("EN");
-
-  const options = ["EN", "UZ", "RU"];
-
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const handleSelect = (option) => {
-    setSelected(option);
-    setIsOpen(false);
-  };
-
-  const menu = () => {
-    let men = document.getElementById('menu');
-    men.classList.replace('hidden')
-  }
+const Navbar = () => {
+  const [dropdown, setDropdown] = useState(false);
 
   return (
-    <div className="Navbar">
-      <ul>
-        <a href="#">
-          <img src={logo} alt="logo" className="logo" />
-        </a>
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <ul className="navbar-links">
+        <li>
+          <a href="#home">About Us</a>
+        </li>
+        <li>
+          <a href="#about">Team</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#questions">Questions</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+        <li
+          className="dropdown"
+          onMouseEnter={() => setDropdown(true)}
+          onMouseLeave={() => setDropdown(false)}
+        >
+          <a href="#more">Language</a>
+          {dropdown && (
+            <ul className="dropdown-menu">
+              <li>
+                <a href="#en">EN</a>
+              </li>
+              <li>
+                <a href="#ru">RU</a>
+              </li>
+              <li>
+                <a href="#uz">UZ</a>
+              </li>
+            </ul>
+          )}
+        </li>
+        <li>
+          <a href="#share">Share</a>
+        </li>
       </ul>
-
-      <button className="menu" id="menu">
-        <div></div>
-        <div></div>
-        <div></div>
-      </button>
-
-      <ul>
-        <li>
-          <a href="#">About Us</a>
-        </li>
-        <li>
-          <a href="#">Team</a>
-        </li>
-        <li>
-          <a href="#">Services</a>
-        </li>
-        <li>
-          <a href="#">Projects</a>
-        </li>
-        <li>
-          <a href="#">Questions</a>
-        </li>
-        <li>
-          <a href="#">Contacts</a>
-        </li>
-      </ul>
-
-      <ul>
-        <li>
-          <div className="dropdown">
-            <a onClick={toggleDropdown} className="dropdown-button">
-              {selected} <span>{isOpen ? "▲" : "▼"}</span>
-            </a>
-
-            {isOpen && (
-              <div className="dropdown-menu">
-                {options.map((option) => (
-                  <div
-                    key={option}
-                    onClick={() => handleSelect(option)}
-                    className="dropdown-item"
-                  >
-                    {option}
-                  </div>
-                ))}
-              </div>
-            )}
-          </div>
-        </li>
-        <li><a href="#">Share</a></li>
-      </ul>
-    </div>
+      <div className="navbar-toggle" onClick={() => setDropdown(!dropdown)}>
+        ☰
+      </div>
+    </nav>
   );
-}
+};
 
 export default Navbar;
