494d078c717bdc1a329f62143a46c8cfa7bee75e
Diyor <baxacap@gmail.com>
2024-11-10 11:10:04 +0500

    Burger component

---
+++ src/components/Burger/BurgerIcon.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./Burger.module.scss";
+
+export default function BurgerIcon({ openBurger, setOpenBurger }) {
+  console.log(openBurger);
+  return (
+    <div
+      onClick={() => setOpenBurger(!openBurger)}
+      className={styles.toggleBtn}
+    >
+      <button
+        className={`${styles.burger} ${openBurger ? styles.active : ""}`}
+      ></button>
+    </div>
+  );
+}
