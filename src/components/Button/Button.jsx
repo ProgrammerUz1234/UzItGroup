5f74954ab8476fe6b5f411cef56471efa26c10e2
Diyor <baxacap@gmail.com>
2024-11-10 11:11:42 +0500

    Button

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import style from "./Button.module.scss";
+
+export default function Button({ children, width, height, font, ...props }) {
+  const buttonStyle = {
+    position: "relative",
+    display: "flex",
+    outline: "none",
+    alignItems: "center",
+    justifyContent: "center",
+    width: width,
+    height: height,
+    border: "1px solid #6B8C49",
+    fontWeight: "700",
+    fontSize: font,
+    textAlign: "center",
+  };
+
+  return (
+    <button {...props} style={buttonStyle} className={style["btn-5"]}>
+      {children}
+    </button>
+  );
+}
